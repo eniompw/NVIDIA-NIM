46883c30b6c7db506a7244c8c28d20c05c45b477
eniompw <71254710+eniompw@users.noreply.github.com>
2024-07-15 10:45:27 +0000

    simplify

--- rest.py
+++ rest.py
@@ -2,12 +2,10 @@ import requests
 import os
 
 url = "https://integrate.api.nvidia.com/v1/chat/completions"
-headers = {
-    "Authorization": "Bearer " + str(os.environ.get("NV_API_KEY")),
-}
-q = "Write a limerick about the wonders of GPU computing."
-messages = [{"role": "user", "content": q}]
-data = { "messages": messages, "model": "meta/llama3-70b-instruct" }
+headers = {"Authorization": "Bearer " + str(os.environ.get("NV_API_KEY"))}
+query = "Write a limerick about the wonders of GPU computing."
+messages = [{"role": "user", "content": query}]
+data = {"messages": messages, "model": "meta/llama3-70b-instruct"}
 
 response = requests.post(url, headers=headers, json=data)
 print(response.json()["choices"][0]["message"]["content"])
